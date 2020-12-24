a335cbf65bce3bc76adf0f7677faf63969b424f2
Sgeo <sgeoster@gmail.com>
2020-12-24 00:44:22 -0500

    "Mounting" as writing to localmount.txt

--- Data/Extensions/AutoMount/main.js
+++ Data/Extensions/AutoMount/main.js
@@ -2,40 +2,20 @@ module.exports.activate = () => {
     const flashpoint = require("flashpoint-launcher");
     const fs = require("fs");
     const util = require("util");
-    const child_process = require("child_process");
 
     const fsAccess = util.promisify(fs.access);
-
-    let alreadyLaunched = new Set();
+    const fsWriteFile = util.promisify(fs.writeFile);
 
     flashpoint.games.onWillLaunchGame(async (gameLaunchInfo) => {
         let id = gameLaunchInfo.game.id;
 
-        if(alreadyLaunched.has(id)) {
-            return;
-        }
-        alreadyLaunched.add(id);
 
         try {
             await fsAccess(`..\\Games\\${id}.zip`);
+            await fsWriteFile("..\\Legacy\\htdocs\\localmount.txt", `mount /localmount/${id}.zip`, "utf8");
         } catch(e) {
             flashpoint.log.info("No GameZIP detected.");
-            return;
+            await fsWriteFile("..\\Legacy\\htdocs\\localmount.txt", ``, "utf8");
         }
-
-        let fpmount = child_process.spawn("..\\FPSoftware\\fpmount\\fpmount", [id]);
-        fpmount.stdout.on("data", data => flashpoint.log.info(`FPMount output: ${data}`));
-        fpmount.stderr.on("data", data => flashpoint.log.info(`FPMount error: ${data}`));
-        await new Promise((resolve, reject) => {
-            fpmount.on("close", code => {
-                if(code) {
-                    flashpoint.log.info(`FPMount failed with error code: ${code}`);
-                    reject();
-                } else {
-                    flashpoint.log.info(`FPMount succeeded.`);
-                    resolve();
-                }
-            })
-        });
     });
 };
