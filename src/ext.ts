e2108f82d5d8610b82c825f9a24b79ae851d150c
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-02-10 14:33:09 -0500

    Moved to promises, added nudge, and commented.
    The promises ensure that AutoMount will block until the mounting is
    complete. This should (in theory) prevent games from getting 404s
    because the mounting is too slow.
    
    The nudges: add some complicated timer logic, and maybe all your
    problems will be solved. Current nudge-timeout is set at two seconds.
    
    Comments: I added some, because I didn't want to force a reader
    to try to understand the monstrosity that this is becoming.

--- src/ext.ts
+++ src/ext.ts
@@ -33,58 +33,150 @@ export async function activate(context: flashpoint.ExtensionContext) {
     return arr;
   }
 
-  async function sleep(ms: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+  // Connects qmp to the given port number and host.
+  const qmpConnect = (qmp, port: number, host: string) => {
+    // Wrap the callback in a promise.
+    return new Promise((resolve, reject) => {
+      // Connect to the port and host.
+      qmp.connect(port, host, (err: Error) => {
+        // If we get an error, reject the promise.
+        if (err) return reject(err);
+        // If we don't get an error, return qmp.
+        resolve(qmp);
+      });
     });
   }
 
-  async function mountGame(id: string, filePath: string) {
-    if(alreadyLaunched.has(id)) {
-      return;
-    }
-    alreadyLaunched.add(id);
-    if (dockerGZ) {
-      flashpoint.log.info("FPMount output: urlopen");
-      request({host: '127.0.0.1', port: '22500', path: `/mount.php?file=${encodeURIComponent(basename(filePath))}`}, function(response) {
+  // Executes a command on a given qmp connection.
+  const qmpExecute = (qmp, funcname: string, argsdict) => {
+    // Wrap the callback in a promise.
+    return new Promise((resolve, reject) => {
+      // Execute funcname with argsdict as its argument.
+      qmp.execute(funcname, argsdict, (err: Error) => {
+        // If we get an error, reject the promise.
+        if (err) return reject(err);
+        // If we don't get an error, return qmp.
+        resolve(qmp);
+      });
+    });
+  }
+
+  // A promise to send an http request. It doesn't have to be to
+  // mount.php, but that's all it's used for, so the name is fine.
+  // qmp will be a nonsense argument if we're on docker,
+  // so we type it as any.
+  const callPHP = (argsdict, qmp: any) => {
+    // Wrap the request's callback in a promise.
+    return new Promise((resolve, reject) => {
+      // Make a request, as specified by argsdict.
+      request(argsdict, (response) => {
+        // Inititalize the response string to empty.
         var str: string = '';
-        response.on('data', function (chunk: string) {
+        // Whenever we get a chunk of data, add it to the response string.
+        response.on('data', (chunk: string) => {
           str += chunk;
         });
-        response.on('end', function () {
-          flashpoint.log.info(`FPMount output: ${str}`);
+        // When the connection ends, resolve the promise.
+        response.on('end', () => {
+          // Return a dictionary with str -> (response string), and qmp -> qmp.
+          // Be aware: if we're on docker, qmp will be nonsense, like one.
+          resolve({"str":str, "qmp": qmp});
         });
       }).end();
+    });
+  }
+
+  // A function to nudge QMP back into responsiveness.
+  async function nudgeQMP() {
+    // We create an object for the new connection.
+    let nudgeConn: QMP = new QMP();
+    // Connect to 4445 (a separate connection), and send a simple command.
+    await qmpConnect(nudgeConn, 4445, '127.0.0.1')
+    .then((nudgeConn: QMP) => qmpExecute(nudgeConn, 'query-block-jobs', {}))
+    .then((nudgeConn: QMP) => {
+      // End the connection.
+      nudgeConn.end();
+    });
+  }
+
+  // Mount a game, if applicable.
+  async function mountGame(id: string, filePath: string) {
+    // If this game is already mounted, don't bother.
+    if (alreadyLaunched.has(id)) {
+      return;
+    }
+    // Add this game to the list of mounted ones.
+    alreadyLaunched.add(id);
+    // If we're on docker, follow docker steps.
+    if (dockerGZ) {
+      // Docker only needs to send a request to mount.php. The second argument (1) is complete nonsense. We just need two arguments.
+      await callPHP({host: '127.0.0.1', port: '22500', path: `/mount.php?file=${encodeURIComponent(basename(filePath))}`}, 1)
+      .then((dict) => {
+        // We did it! Log whatever mount.php returned.
+        flashpoint.log.info(`mount.php returns: ${dict['str']}`);
+      }).catch((err) => {throw err;});
     } else {
-      flashpoint.log.info(`FPMount output: Mounting ${id}`);
+      // A variable to track whether the main task has completed.
+      // At this point, the main task has not completed.
+      let completed: boolean = false;
+      // Send a log message: we're about to begin mounting.
+      flashpoint.log.info(`Mounting ${id}`);
       // Generate a random 16-character string.
       let drive: string = randomBytes(16).map(function(element) { return (element % 26) + 97; }).toString();
-      // Convert the uuid to bytes, then encode it with base85.
+      // Convert the uuid to bytes, then encode it with ascii85.
       let data: Uint8Array = uuidToBytes(id);
       let serial: string = Ascii85.encode(data).toString('ascii');
+      // Create an object for the QMP connection.
       let qmp: QMP = new QMP();
-      // This library wants the port before the host?
-      qmp.connect(4444, '127.0.0.1', function(err: Error) {
-        if (err) throw err;
-        qmp.execute('blockdev-add', {'node-name': drive, 'driver': 'raw', 'file': { 'driver': 'file', 'filename': filePath}}, function(err: Error) {
-          if (err) throw err;
-          qmp.execute('device_add', {'driver': 'virtio-blk-pci', 'drive': drive, 'id': drive, 'serial': serial}, function(err: Error) {
-            if (err) throw err;
-            flashpoint.log.info("FPMount output: urlopen");
-            request({host: '127.0.0.1', port: '22500', path: `/mount.php?file=${encodeURIComponent(serial)}`}, function(response) {
-              var str: string = '';
-              response.on('data', function (chunk: string) {
-                str += chunk;
-              });
-              response.on('end', function () {
-                flashpoint.log.info(`FPMount output: ${str}`);
-              });
-            }).end();
-            flashpoint.log.info('FPMount succeeded.');
-            qmp.end();
-          });
-        });
+
+      // The main task will connect, execute two commands, and send a request to mount.php.
+      // Note: this is just a promise to complete the main task. We will ensure that it delivers later.
+      let mainTask = qmpConnect(qmp, 4444, '127.0.0.1')
+      .then((qmp) => qmpExecute(qmp, 'blockdev-add', {'node-name': drive, 'driver': 'raw', 'file': { 'driver': 'file', 'filename': filePath}}))
+      .then((qmp) => qmpExecute(qmp, 'device_add', {'driver': 'virtio-blk-pci', 'drive': drive, 'id': drive, 'serial': serial}))
+      .then((qmp) => callPHP({host: '127.0.0.1', port: '22500', path: `/mount.php?file=${encodeURIComponent(serial)}`}, qmp))
+      .then((dict) => {
+        // When we're done with all that, log whatever mount.php returned.
+        flashpoint.log.info(`mount.php returns: ${dict['str']}`);
+        // Close the qmp connection. We gave callPHP a real qmp argument, so we'll be
+        // getting back the real qmp from this.
+        dict['qmp'].end();
+        // Set the flag: we've completed the main task.
+        completed = true;
+      }).catch((err) => {throw err;});
+
+      // We also have a secondary task: the timer task.
+      let timerPromise = new Promise((resolve, reject) => {
+        // This is an anonymous asynchronous recursive function. Say that five times fast.
+        // Arguments: the amount of time to wait before recursing, how deep into the recursion we are, how
+        // deep we're allowed to go before we should begin to nudge, and the callback to call when we're done.
+        (async function timer(waitTime, depth, maxDepth, callback) {
+          // Is the main task still incomplete?
+          if (!completed) {
+            // Yes. Should we send a nudge?
+            if (depth > maxDepth) {
+              // Yes, send one. Wait for that to complete.
+              await nudgeQMP();
+            }
+            // Recurse (kinda) after waiting waitTime. Pass through all the arguments
+            // untouched except depth, which is incremented by one.
+            setTimeout(timer, waitTime, waitTime, depth + 1, maxDepth, callback);
+          } else {
+            // Oh look, the main task finished! Call the callback.
+            // Note: it required that I supply it with a return value, so here's
+            // a nonsense value to return.
+            callback(1)
+          }
+        // We call this lovely function with the arguments:
+        //   waitTime = 200ms     It seemed about right.
+        //   depth = 0            We're starting off with zero recursions.
+        //   maxDepth = 10        If it takes longer than two seconds, begin nudging.
+        //   callback = resolve   When the main task is over, then we resolve the promise.
+        })(200, 0, 10, resolve);
       });
+      // Evaluate the two promises simultaneously.
+      // timerPromise waits on mainTask's completion before resolving, so mainTask will always be first.
+      await Promise.race([mainTask, timerPromise]);
     }
   }
 
